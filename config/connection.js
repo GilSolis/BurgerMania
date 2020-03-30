91b7f27ca200fbba4542d5a76dd88dd3b6f49c96
Gil Solis <gilsolisuw@gmail.com>
2020-03-29 22:29:35 -0700

    typo in connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 var mysql = require("mysql");
+let connection;
 
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -10,15 +11,16 @@ if (process.env.JAWSDB_URL) {
         password: "password1",
         database: "burgers_db"
     });
+}
 
-    // Connect to the database
-    connection.connect(function(err) {
-        if (err) {
-            console.error("error connecting: " + err.stack);
-            return;
-        }
-        console.log("connected as id " + connection.threadId);
-    });
+// Connect to the database
+connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 
-    // Export connection
-    module.exports = connection;
+// Export connection
+module.exports = connection;
